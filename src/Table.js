0314d12db2e06963b13adcaeed429e7334d28289
ArshWalker <arshwalker7@gmail.com>
2021-07-11 07:51:06 +0530

    Table.js added

---
+++ src/Table.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
+
+function Table({ countries }) {
+  //destructing within the actual bcz we have to use it for 2*td
+  return (
+    <div className="table">
+      {countries.map(({ country, cases, countryInfo }) => (
+        <tr>
+          <td>
+            <div className="info-fla">
+              <img
+                src={countryInfo.flag}
+                style={{ height: "26px", width: "38px" }}
+                alt=""
+              />
+            </div>
+          </td>
+          <td>{country}</td>
+          <td>
+            <strong>{numeral(cases).format("0,0")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
